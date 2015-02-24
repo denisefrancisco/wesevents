a1a246674847ebb8706f9d18bc592ce8fdd2d420
denisefrancisco <cfrancisco@wesleyan.edu>
2015-02-23 19:18:19 -0500

    database restructuring

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //javascript database handling and creating the firebase database
 var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://wesevents.firebaseio.com/");
+/*var myFirebaseRef = new Firebase("https://wesevents.firebaseio.com/");
 
 myFirebaseRef.set({
     title: "Hello World!",
@@ -14,4 +14,12 @@ myFirebaseRef.set({
 
 myFirebaseRef.child("location/city").on("value", function(snapshot) {
     alert(snapshot.val()); //Alerts "San Francisco"
+});*/
+
+var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
 });
