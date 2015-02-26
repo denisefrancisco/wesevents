fe27a9a17f1ea38c1793e8c9596db2f506b34a06
denisefrancisco <cfrancisco@wesleyan.edu>
2015-02-26 17:19:30 -0500

    using wesleyan API

--- page_function.js
+++ page_function.js
@@ -6,7 +6,16 @@ function submitEvent(){
         console.log("weeee");
     }
 
-window.onload = function() {
-    
-    
+//using the Wesleyan API for current events
+function get_events() {
+    $("masterList").getJSON("http://wesapi.org/api/events/today", function(res) {
+        if (!(res)) {
+            console.log("No response");
+        } else {
+            console.log("Got response");
+            console.log(res)
+        }
+    console.log("YES IT WORKED!!!!!")
+    });
 }
+
