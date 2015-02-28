4420509a8118cd4e0aa6ba19cf3c98f1cc9bef4b
denisefrancisco <cfrancisco@wesleyan.edu>
2015-02-28 11:37:12 -0500

    submit button redirects to a new page

--- page_function.js
+++ page_function.js
@@ -11,27 +11,32 @@ function submitEvent(){
         var description = document.getElementById("description").value;
         var tags = document.getElementById("tags").value;
         console.log(names);
+        console.log(times);
     }
 
 //using the Wesleyan API for current events
 $("#masterList").ready(function() {
     console.log("hi");
     $.getJSON("http://wesapi.org/api/events/today", function(res) {
+
         if (!(res)) {
             console.log("No response");
         } else {
             console.log("Got response");
             var elist = $("#eventList");
             res.Results.forEach(function(event){
-                elist.append("<li><a href='#'>"+ event.name + "</a></li>");
+                elist.append("<li><a href='#'>"+ event.name + "</a></li>");  
+
             });
-            elist.listview("refresh");    
-            
             console.log(res);
+            elist.listview("refresh");
         }
+
     console.log("YES IT WORKED!!!!!");
     });
 });
+
+
 
 window.onload = function() {
     jQuery('#datetimepicker').datetimepicker();
