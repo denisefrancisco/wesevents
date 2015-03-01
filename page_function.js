c8466a22adfb6292d6a59ffc20c122d3409fe1e1
Dong <Dong@DongdeMacBook-Pro.local>
2015-02-28 22:39:47 -0500

    WE GOT THIS SHIT TO WORK YE YEEEEEEEEE -Donovan

--- page_function.js
+++ page_function.js
@@ -5,11 +5,11 @@ function changeTheText(){
 function submitEvent(){
         console.log("weeee");
         var names = document.getElementById("name").value;
-        var times = document.getElementById("datetimepicker").value;
-        var places = document.getElementById("place").value;
+        var time = document.getElementById("datetimepicker").value;
+        var place = document.getElementById("place").value;
         var host = document.getElementById("creator").value;
         var description = document.getElementById("description").value;
-        var tags = document.getElementById("tags").value;
+        var tag = document.getElementById("tags").value;
         console.log(names);
         console.log(times);
 
@@ -28,19 +28,24 @@ $(document).on("pagebeforeshow","#masterList",function() {
             console.log("Got response");
             
             res.Results.forEach(function(event){
-                elist.append("<li><a href='#descriptionPage' data-role = 'button'>"+ event.name + "</a></li>");  
+                var collapsible = "<div data-role ='collapsible' ><h3> "+ event.name +  "</h3> <p>" + event.location + "<br>" + event.time + "<br>" 
+                                    + event.description + "<br>" +"</p></div>";
+                elist.append($(collapsible).collapsible());
 
             });
             console.log(res);
-            elist.listview("refresh");
+            //elist.collapsible("refresh");
         }
 
         myFirebaseRef.on("value", function(snapshot) {
             console.log(snapshot.val());
             $.each(snapshot.val().events, function(key, event) {
-                elist.append("<li><a href='#descriptionPage' data-role = 'button'>"+ event.names + "</a></li>");  
+                var collapsible = "<div data-role ='collapsible'><h3> "+ event.names +  "</h3>" +
+                                    "<p>" + event.place + "<br>" + event.time + "<br>" + event.creator + "<br>" +
+                                     event.description + "<br>" + event.tag +"</p></div>";
+                elist.append($(collapsible).collapsible());
             });
-            elist.listview("refresh");
+            elist.collapsible("refresh");
         }, function (errorObject) {
             console.log("The read failed: " + errorObject.code);
         });
