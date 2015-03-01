6a7bc2381f7b74c8ce1765d03e2baa88dd65c8e9
Donovan Brady <dbrady@wesleyan.edu>
2015-03-01 10:51:07 -0500

    made dat shit PURTY

--- page_function.js
+++ page_function.js
@@ -28,8 +28,12 @@ $(document).on("pagebeforeshow","#masterList",function() {
             console.log("Got response");
             
             res.Results.forEach(function(event){
-                var collapsible = "<div data-role ='collapsible' ><h3> "+ event.name +  "</h3> <p>" + event.location + "<br>" + event.time + "<br>" 
-                                    + event.description + "<br>" +"</p></div>";
+                var what = "<b> Name: </b>"
+                var where = "<b> Place: </b>"
+                var time = "<b> Time: </b>"
+                var descript ="<b> Description: </b>"
+                var collapsible = "<div data-role ='collapsible' ><h3> " + event.name +  "</h3> <p>" + what + event.name + "<br>" + "<br>" + where + 
+                                event.location + "<br>" + "<br>" + time + event.time + "<br>" + "<br>" + descript + event.description + "<br>" +"</p></div>";
                 elist.append($(collapsible).collapsible());
 
             });
@@ -40,9 +44,15 @@ $(document).on("pagebeforeshow","#masterList",function() {
         myFirebaseRef.on("value", function(snapshot) {
             console.log(snapshot.val());
             $.each(snapshot.val().events, function(key, event) {
+                var what = "<b> Name: </b>"
+                var where = "<b> Place: </b>"
+                var time = "<b> Time: </b>"
+                var descript = "<b> Description: </b>"
+                var madeby = "<b> Creator: </b>"
+                var cat = "<b> Tags: </b>"
                 var collapsible = "<div data-role ='collapsible'><h3> "+ event.names +  "</h3>" +
-                                    "<p>" + event.place + "<br>" + event.time + "<br>" + event.creator + "<br>" +
-                                     event.description + "<br>" + event.tag +"</p></div>";
+                                    "<p>" + what + event.names + "<br>" + "<br>" + where + event.place + "<br>" + "<br>" + time + event.time + "<br>" + "<br>" + madeby + event.creator + "<br>" +
+                                     "<br>" + descript + event.description + "<br>" + "<br>" + cat + event.tag +"</p></div>";
                 elist.append($(collapsible).collapsible());
             });
             elist.collapsible("refresh");
